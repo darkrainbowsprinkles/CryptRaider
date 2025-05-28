dc7c92c32d11af106009fe03ba749eac3fdef637
Octavio Ortega <octaviortegan@hotmail.com>
2025-05-28 11:48:30 -0600

    Trigger Overlap Array for SecretWall

---
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)\
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    TArray<AActor*> Actors;
+
+    GetOverlappingActors(Actors);
+
+    if(Actors.Num() > 0)
+    {
+        UE_LOG(LogTemp, Display, TEXT("%s"), *Actors[0]->GetActorNameOrLabel());
+    }
+}
---
+++ Source/CryptRaider/TriggerComponent.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "TriggerComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CRYPTRAIDER_API UTriggerComponent : public UBoxComponent
+{
+	GENERATED_BODY()
+
+public:
+	UTriggerComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
